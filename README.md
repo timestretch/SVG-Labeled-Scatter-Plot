db24a98351ecd831aca913444bbc64d5cc2bf8d1
Erik Wrenholt <erik@timestretch.com>
2016-03-13 17:30:27 -0500

    init

---
+++ README.md
@@ -0,0 +1,6 @@
+SVG Labeled Scatter Plot
+------------------------
+
+This is a scatter plot report in Ruby and Sinatra. It features labeled values, and a tooltip to show the x and y values when you hover over a dot.
+
+2016-03-13 Erik Wrenholt
---
+++ app.rb
@@ -0,0 +1,19 @@
+require 'sinatra'
+require './report'
+
+class App < Sinatra::Base
+
+	get '/' do
+
+        data = []
+        (0..25).each do |i|
+            # [ x, y, label ]
+            data << [i, (Math.sin(i/5.0) * 1000).to_i, (i+65).chr]
+        end
+
+		report = Report.new.render(data)
+
+        erb :home, :locals => {:svg => report}
+
+	end
+end
